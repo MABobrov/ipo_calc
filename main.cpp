62d9be69e94aff0741a97007b938c38bc923297a
MABobrov <118075278+MABobrov@users.noreply.github.com>
2023-03-14 20:37:27 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -22,13 +22,13 @@ int main() {
 	printf("|-------------|------------|--------------------------|--------------|\n");
 	double balance = creditSum + (creditSum * monthRate);
 	// расчет и вывод каждого месячного платежа до полного погашения кредита
-	for(year = 2023; balance > 0.000; year++) {
-		for(month = 1; (month < 13&&balance > 0); month++) {
+	for(year=2023; balance > 0.000; year++) {
+		for(month = 1; (month < 13&&balance >0); month++) {
 			double monthPayment = fmin(balance + (balance * monthRate), initPayment + (balance * monthRate)); // расчет ежемесячного платежа
 			double sumRate = balance * monthRate; // расчет суммы процентов
 			printf("| %02d/%d  |  %9.2lf  |  %24.2lf  |  %10.2lf  |\n", month, year, monthPayment, balance, sumRate); // вывод информации о платеже
-			balance -= monthPayment; // расчет остатка задолженности после платежа}
+			balance -= (monthPayment+sumRate);  // расчет остатка задолженности после платежа}
 	}
 
 	return 0;
-}
+}}
